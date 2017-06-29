b83cc56d5095f06f3392a6bfb4a75bfc1e28283d
Ernst-Paul ten Brinke <eptb@schubergphilis.com>
2017-06-29 02:37:38 +0200

    Improve read-data-from-microbit

--- read-data-from-microbit.py
+++ read-data-from-microbit.py
@@ -11,17 +11,15 @@ s.parity   = serial.PARITY_NONE
 s.databits = serial.EIGHTBITS
 s.stopbits = serial.STOPBITS_ONE
 
-try:
-    while True:
-        # read a line from the microbit and decode it
+while True:
         data = s.readline().decode('UTF-8')
-
-        # strip whitespaces and split the accelerometer and button data into x, y, z, a, b
         data_s = data.rstrip().split(' ')
-        x, y, z = data_s[0], data_s[1], data_s[2]
-        a, b = data_s[3], data_s[4]
-        print(x,y,z,a,b)
+        try:
+            x, y, z, a, b = data_s
+            print(x,y,z,a,b)
 
-finally:
-    s.close()
+        except:
+            pass
+
+s.close()
 
